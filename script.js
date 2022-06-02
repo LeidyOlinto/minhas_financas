60233c254a7a04a590227f681c2e1e71cb3430c9
leidy olinto <suverleide.olinto@gmail.com>
2022-06-02 16:51:28 -0300

    alterações

--- script.js
+++ script.js
@@ -12,43 +12,43 @@ if (transacaoRaw != null) {
 }
 console.log(transacoes);
 function envioFormulario(e) {
-
     console.log(e);
 }
 function desenhaTabela() {
-//Tabela dinamica.
+    //Tabela dinamica.
     total = 0;
     for (linha of transacoes) {
         linhasTransacao += `
-     <tr>
-         <td>${linha.tipo} ${linha.nome}</td>  
-         <td class="valor">R$ ${linha.valor.toLocaleString('pt-BR')}</td>
-      </tr>
-            `;
-     total += linha.valor * (linha.tipo == '-' ? -1 : 1)
+            <tr>
+                <td>${linha.tipo} ${linha.nome}</td>  
+                <td class="valor">R$ ${linha.valor.toLocaleString('pt-BR',{minimumFractionDigits:2})}</td>
+            </tr>
+        `;
+        total += linha.valor * (linha.tipo == '-' ? -1 : 1)
     }
     totalSemsinal = total * (total >= 0 ? 1 : -1)
     if (transacoes.length == 0) {
         totalString = `<h4 style="text-align: center;
         display: flow-root;">Nenhuma transação adicionada</h4>`;
     } else {
-        totalString = `
-    <thead>
-     <tr>
-        <th class="mercadoria-esquerda">Mercadoria</th>
-        <th class="valor">Valor</th>
-     </tr>
-    </thead>
-     ${linhasTransacao}
-     <tr>
-      <td colspan="3" class="vt" id="lc"></td>
-    </tr>
-    <tr>
-     <td class="total-esquerda">Total</td>
-     <td class="valor-final">
-    R$ ${totalSemsinal.toLocaleString('pt-BR')}
-    </br>
-    `
+        totalString =
+            `
+                <thead>
+                    <tr>
+                        <th class="mercadoria-esquerda">Mercadoria</th>
+                        <th class="valor">Valor</th>
+                    </tr>
+                </thead>
+                ${linhasTransacao}
+                <tr>
+                    <td colspan="3" class="vt" id="lc"></td>
+                </tr>
+                <tr>
+                    <td class="total-esquerda">Total</td>
+                    <td class="valor-final">
+                        R$ ${totalSemsinal.toLocaleString('pt-BR',{minimumFractionDigits:2})}
+                        </br>
+            `
         if (total > 0) {
             totalString += '<a style=" font-size: 10px;">[LUCRO]</a>'
         }
@@ -56,9 +56,9 @@ function desenhaTabela() {
             totalString += '<a style=" font-size: 10px;">[PREJUIZO]</a>'
         }
         totalString += `
-      </td>
-   </tr>             
-`;
+            </td>
+        </tr>             
+            `;
     }
     document.getElementById('tabela').innerHTML += totalString
 }//limpar os dados da transação.
@@ -86,10 +86,10 @@ function addItem(item) {
     localStorage.setItem("transacoes", JSON.stringify(transacoes));
 }
 //Aqui fecha a aba do modal para telas de tablete e celular.
-function fechaModal(){
-    document.getElementsByClassName('modal-fechar')[0].style.display='none';
+function fechaModal() {
+    document.getElementsByClassName('modal-fechar')[0].style.display = 'none';
 }
-function abreModal(){
-    document.getElementsByClassName('modal-fechar')[0].style.display='block';
-    
+function abreModal() {
+    document.getElementsByClassName('modal-fechar')[0].style.display = 'block';
+
 }
